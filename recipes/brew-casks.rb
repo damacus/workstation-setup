8bcc270c14aaaf90ba73a072352cf2363b6f1515
Dan Webb <dan.webb@damacus.io>
2018-11-24 09:39:28 +0000

    Add taps and gists

--- recipes/brew-casks.rb
+++ recipes/brew-casks.rb
@@ -1,5 +1,7 @@
-cask = %w(discord atom visual-studio-code hab
-          java spectacle aws-vault numi vagrant docker google-cloud-sdk
+brew_tap 'wata727/tflint'
+
+cask = %w(discord atom visual-studio-code docker hab
+          java spectacle aws-vault numi vagrant google-cloud-sdk
           slack virtualbox steam slack twitch whatsapp authy
           iterm2 google-chrome google-backup-and-sync chef/chef/chef-workstation)
 
