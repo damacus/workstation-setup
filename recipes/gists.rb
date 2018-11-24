25f5e4b7378d1063ca0ac879d7afa1acd5767184
Dan Webb <dan.webb@damacus.io>
2018-11-24 10:04:20 +0000

    Fix modes on scripts

--- recipes/gists.rb
+++ recipes/gists.rb
@@ -2,14 +2,16 @@ git 'brew-update' do
   repository 'https://gist.github.com/81f128c3c6fd10a7d1295cf612282bb0.git'
   destination ::File.join('Users', node['user'], 'brew-update.sh')
   depth 1
-  user          node['user']
-  group         node['group']
+  user node['user']
+  group node['group']
+  mode '0755'
 end
 
 git 'vagrant-box-update' do
   repository 'https://gist.github.com/5f097adb3937c7340013f79cb1221c4d.git'
   destination ::File.join('Users', node['user'], 'box-update.sh')
   depth 1
-  user          node['user']
-  group         node['group']
+  user node['user']
+  group node['group']
+  mode '0755'
 end
