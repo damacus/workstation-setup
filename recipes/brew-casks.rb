20cfbaf4d46ddbe652645c98a0ab23151a710dd4
Dan Webb <dan.webb@damacus.io>
2018-11-24 09:54:10 +0000

    Add Firefox to casks

--- recipes/brew-casks.rb
+++ recipes/brew-casks.rb
@@ -2,7 +2,7 @@ homebrew_tap 'wata727/tflint'
 
 cask = %w(discord atom visual-studio-code docker hab
           java spectacle aws-vault numi vagrant google-cloud-sdk
-          slack virtualbox steam slack twitch whatsapp authy
+          slack virtualbox steam slack twitch whatsapp authy firefox
           iterm2 google-chrome google-backup-and-sync chef/chef/chef-workstation)
 
 
