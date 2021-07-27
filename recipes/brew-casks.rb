58c58c497bcf9e7401ad12e7e25338622dd28397
Dan Webb <dan.webb@damacus.io>
2021-07-27 10:11:29 +0100

    Remove casks we no longer use

--- recipes/brew-casks.rb
+++ recipes/brew-casks.rb
@@ -1,9 +1,9 @@
 homebrew_tap 'wata727/tflint'
 
-cask = %w(discord atom visual-studio-code docker
-          java spectacle aws-vault numi vagrant google-cloud-sdk
-          slack virtualbox steam slack twitch whatsapp authy firefox
-          iterm2 google-chrome google-backup-and-sync chef/chef/chef-workstation)
+cask = %w(discord visual-studio-code docker
+          spectacle aws-vault vagrant slack 
+          slack whatsapp authy firefox iterm2 
+          google-chrome google-drive chef/chef/chef-workstation)
 
 cask.each do |c|
   homebrew_cask c
