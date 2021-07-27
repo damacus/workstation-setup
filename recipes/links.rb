bb05a667851eee982272cf5c266ebbb1d24b202b
Dan Webb <dan.webb@damacus.io>
2021-07-27 10:34:07 +0100

    Update links.rb
    Update location for new Google Drive location

--- recipes/links.rb
+++ recipes/links.rb
@@ -1,8 +1,8 @@
 %w(
-  .zshrc .zshrc.d .aws .oh-my-zsh .ssh .zgen-setup .stove
+  .zshrc .zshrc.d .aws .oh-my-zsh .ssh .zgen-setup
   .git_commit_msg.txt .gitconfig .gitignore zgen .direnvrc
 ).each do |link|
-  link ::File.join('Users', node['user'], 'google_drive', link) do
+  link ::File.join('Users', node['user'], 'Google Drive', 'My Drive', link) do
     to ::File.join('Users', node['user'], link)
   end
 end
