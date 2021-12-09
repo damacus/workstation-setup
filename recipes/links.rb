def4b0755d96e9eb1c9384024b174c4c92641aff
Dan Webb <dan.webb@damacus.io>
2021-12-09 09:44:35 +0000

    Fix Google Drive location
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- recipes/links.rb
+++ recipes/links.rb
@@ -2,7 +2,7 @@
   .zshrc .zshrc.d .aws .oh-my-zsh .ssh .zgen-setup
   .git_commit_msg.txt .gitconfig .gitignore zgen .direnvrc
 ).each do |link|
-  link ::File.join('Users', node['user'], 'Google Drive', 'My Drive', link) do
+  link ::File.join('Volumes', 'GoogleDrive', 'My Drive', link) do
     to ::File.join('Users', node['user'], link)
   end
 end
